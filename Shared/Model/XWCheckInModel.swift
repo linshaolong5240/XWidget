e7cb651a5d1b028e3ebec6fc647d3480bd72d271
teenloong <634205468@qq.com>
2022-05-16 19:14:08 +0800

    XWCheckInRepeat

---
+++ Shared/Model/XWCheckInModel.swift
@@ -0,0 +1,72 @@
+//
+//  XWCheckInModel.swift
+//  XWidget (iOS)
+//
+//  Created by tenloong on 2022/5/16.
+//  Copyright © 2022 com.teenloong.com. All rights reserved.
+//
+
+import Foundation
+
+enum XWCheckInRepeat: Codable, Equatable {
+    case day
+    case week(firstWeek: Int)
+    case month
+    
+    func dateInterval(from date: Date) -> DateInterval {
+        var calendar = Calendar.current
+        switch self {
+        case .day:
+            return Calendar.current.dateInterval(of: .day, for: date)!
+        case .week(let firstWeek):
+            calendar.firstWeekday = firstWeek
+            return Calendar.current.dateInterval(of: .weekOfMonth, for: date)!
+        case .month:
+            return Calendar.current.dateInterval(of: .month, for: date)!
+        }
+    }
+}
+
+struct XWCheckInModel {
+    var createDate: Date = Date()
+    var dateInterval: DateInterval
+    
+    var title: String
+    var currentNumber: Int
+    var targetNumber: Int
+    var checkInRepeat: XWCheckInRepeat
+    var kind: String
+    
+    var isCheckInValid: Bool { dateInterval.contains(Date()) }
+    var isComplete: Bool { currentNumber == targetNumber }
+
+    init(createDate: Date, tilte: String, currentNumber: Int, targetNumber: Int, checkInRepeat: XWCheckInRepeat, kind: String) {
+        self.createDate = createDate
+        self.title = tilte
+        self.currentNumber = currentNumber
+        self.targetNumber = targetNumber
+        self.checkInRepeat = checkInRepeat
+        self.kind = kind
+        self.dateInterval = checkInRepeat.dateInterval(from: createDate)
+    }
+    
+    mutating func resetCheckIn() {
+        createDate = Date()
+        currentNumber = 0
+    }
+    
+    mutating func checkIn() {
+        guard isCheckInValid else {
+            resetCheckIn()
+            currentNumber += 1
+            return
+        }
+        
+        guard currentNumber < targetNumber else {
+            return
+        }
+        
+        currentNumber += 1
+    }
+    
+}
